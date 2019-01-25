b3503b6d470b4e831876aa30b041f979a528c7ea
gabriel.menacho <Gabriel.Menacho@coxautoinc.com>
2019-01-25 13:23:23 -0400

    fix: code style feat: install dependencies for auth0

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,14 +9,9 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View>
-        <Button title="Show me more of the app" onPress={this._showMoreApp} />
         <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
       </View>
     )
-  }
-
-  _showMoreApp = () => {
-    this.props.navigation.navigate('Other')
   }
 
   _signOutAsync = async () => {
